ce1515be9dddc840c640f169a393552d97d3ffbe
Abdulla Al Muhit <muhitabdullah279@gmail.com>
2025-04-16 21:20:13 +0530

    Grid layout has modified.

--- src/app/components/ImageGrid.tsx
+++ src/app/components/ImageGrid.tsx
@@ -42,7 +42,7 @@ export default function ImageGrid() {
           Loading...
         </Typography>
       ) : filteredImages.length ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 sm:gap-x-4">
           {filteredImages.map((img) => (
             <ImageCard
               key={img.id}
